f92fd656e51c52fc6cfad5a64058d03e0498399a
Frédéric Müller <fredericmueller@gmx.net>
2022-03-01 15:25:45 +0100

    Renamed due to issues with the installation via pip

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-import blocks
-import creators
-import losses
-import metrics
-import search_spaces
---
+++ unet-building-toolkit/__init__.py
@@ -0,0 +1,5 @@
+from . import blocks
+from . import creators
+from . import losses
+from . import metrics
+from . import search_spaces
